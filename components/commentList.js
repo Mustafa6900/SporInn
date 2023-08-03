8e06e0e49f3d1d067d96ff72744cf3c35fe9205d
MustafaDemirhan <58307398+Mustafa6900@users.noreply.github.com>
2023-08-03 14:45:15 +0300

    Fitness comment is finished! star rating for error

--- components/commentList.js
+++ components/commentList.js
@@ -1,37 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, FlatList, StyleSheet, TouchableOpacity, Modal } from 'react-native';
 import { FontAwesome } from "react-native-vector-icons";
 import { AntDesign } from '@expo/vector-icons';
+import { supabase } from '../supabaseClient';
 
-const CommentList = () => {
-
-  const comments = [
-    "Bu ürün harika!",
-    "Çok memnun kaldım!",
-    "Bu ürünü çok beğendim!",
-    "Bu ürünü çok beğendim!",
-    "Bu ürünü çok beğendim!",
-    "Bu ürünü çok beğendim!",
-    "Bu ürünü çok beğendim!",
-    "Bu ürünü çok beğendim!",
-    "Bu ürünü çok beğendim!",
-    "Bu ürünü çok beğendim!",
-    "Bu ürünü çok beğendim!",
-  ];
-
-  const point = [
-    "4.8",
-  ]; 
-
+const CommentList = ({ item }) => {
+  const [comments, setComments] = useState([]);
   const [selectedComment, setSelectedComment] = useState(null);
+  const [selectedCommentUser, setSelectedCommentUser] = useState(null);
+  console.log(comments)
+  useEffect(() => {
+    const fetchComments = async () => {
+      const { data: fetchedComments, error } = await supabase
+        .from('comments')
+        .select('*,profiles(first_name,last_name)') 
+        .eq('fitness_centers_id', item.id);
+      if (error) {
+        console.error(error);
+      } else {
+        setComments(fetchedComments);
+      }
+    };
+    fetchComments();
+  }, []);
+
 
   const renderItem = ({ item, index }) => (
-    <TouchableOpacity onPress={() => setSelectedComment(comments[index])}>
+    <TouchableOpacity onPress={() => [setSelectedComment(comments[index].comments_text), setSelectedCommentUser(comments[index].profiles.first_name) ]}>
       <View style={styles.commentContainer}>
-        <FontAwesome name={"comment"} color={"#AAAAAA"} size={20} style={{ marginRight: 10,marginLeft:10 }} />
-        <Text style={styles.commentText}>{item}</Text>
-        <AntDesign name="star" size={24} color="#FF6F25" style={{left:"79%",position:"absolute"}} />
-        <Text style={{fontSize:20,fontWeight:"800",left:"87%",fontFamily:'Roboto',position:"absolute",color:"#AAAAAA"}}>{point}</Text>
+        <FontAwesome name={"comment"} color={"#AAAAAA"} size={20} style={{ marginRight: 10, marginLeft: 10 }} />
+        <Text style={styles.commentText}>{item.comments_text}</Text>
+        <AntDesign name="star" size={24} color="#FF6F25" style={{ left: "79%", position: "absolute" }} />
+     
+        <Text style={{ fontSize: 20, fontWeight: "800", left: "87%", fontFamily: 'Roboto', position: "absolute", color: "#AAAAAA" }}>{item.rating}.0</Text>
       </View>
     </TouchableOpacity>
   );
@@ -48,6 +49,7 @@ const CommentList = () => {
       {/* Modal */}
       <Modal visible={selectedComment !== null} transparent animationType="fade">
         <View style={styles.modalContainer}>
+           <Text style={styles.modalUserText}>{selectedCommentUser}</Text>
           <Text style={styles.modalText}>{selectedComment}</Text>
           <TouchableOpacity onPress={() => setSelectedComment(null)} style={styles.closeButton}>
             <FontAwesome name={"close"} color={"#0D0D0D"} size={40} />
@@ -81,15 +83,25 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
   },
   modalText: {
     fontSize: 18,
     fontWeight: 'bold',
-    color: '#FFFFFF',
+    color: '#EEEEEE',
     padding: 20,
     borderRadius: 10,
     backgroundColor: '#292929',
+  },
+  modalUserText: {
+    fontSize: 20,
+    fontWeight: '500',
+    color: '#FF6F25',
+    padding: 20,
+    borderRadius: 10,
+    backgroundColor: '#292929',
+    top: 10,
+    left: 10,
   },
   closeButton: {
     position: 'absolute',
--- components/sportprofileinfo.js
+++ components/sportprofileinfo.js
@@ -1,14 +1,25 @@
-import React, { useState } from 'react';
-import { View, Image, TouchableOpacity, Text, StyleSheet, ScrollView } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { View, Image, TouchableOpacity, Text, StyleSheet, Modal, TextInput, Alert } from 'react-native';
 import ItemTitleFavorite from './titleFavorite';
 import { FontAwesome } from "react-native-vector-icons";
 import CommentList from './commentList';
 import * as Animatable from 'react-native-animatable';
-import {MaterialIcons} from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { supabase } from '../supabaseClient';
+import { AuthContext } from '../pages/Auth/AuthContext';
+import StarRating from 'react-native-star-rating';
+import { ViewPropTypes } from 'deprecated-react-native-prop-types';
 
 const SportProfileInfo = ({ items }) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [showComments, setShowComments] = useState(false);
+  const [isCommentModalVisible, setIsCommentModalVisible] = useState(false);
+  const [newComment, setNewComment] = useState('');
+  const [rating, setRating] = useState(0);
+  const { session } = useContext(AuthContext);
+
+  const userName = session.user?.user_metadata?.first_name;
+  const lastName = session.user?.user_metadata?.last_name;
 
   const handleImagePress = () => {
     setIsModalVisible(true);
@@ -22,81 +33,147 @@ const SportProfileInfo = ({ items }) => {
     setShowComments(!showComments);
   };
 
+  const handleNewCommentPress = () => {
+    console.log("Yorum Yap");
+    setIsCommentModalVisible(true); // Yorum yap popup'ını açmak için modalı görünür yap
+  };
+
+  const handleNewCommentSubmit = async () => {
+    try {
+        const { data, error } = await supabase.from('comments').insert({
+          created_at: new Date(),
+          fitness_centers_id: items.id,
+          created_id: session.user.id,
+          rating: rating,
+          comments_text: newComment,
+         
+        });
+        if (error) {
+          console.error("Error inserting comment:", error.message);
+        } else {
+          console.log("Comment inserted successfully");
+          Alert.alert("Yorumunuz başarıyla eklendi!");
+        }
+        setNewComment('');
+        setRating(0);
+        setIsCommentModalVisible(false);
+      
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  };
+
+
   return (
     <View>
-         <Animatable.View
-        animation="zoomIn" // İstediğiniz animasyonu buraya ekleyin
-        duration={1000} // Animasyon süresini burada ayarlayın
+      <Animatable.View
+        animation="zoomIn"
+        duration={1000}
         style={styles.container}
       >
-      <Image source={{ uri: items.imageData?.publicUrl }} style={styles.image} />
-      <ItemTitleFavorite title={items.name} point="4.8" />
-      <TouchableOpacity onPress={handleImagePress}>
-        {isModalVisible ? null : ( 
-        <FontAwesome name="angle-double-down" color="#FF6F25" size={30} style={{ left: "47%", bottom: "1%" }} />
-        )}
-      </TouchableOpacity>
+        <Image source={{ uri: items.imageData?.publicUrl }} style={styles.image} />
+        <ItemTitleFavorite title={items.name} point="4.8" />
+        <TouchableOpacity onPress={handleImagePress}>
+          {isModalVisible ? null : (
+            <FontAwesome name="angle-double-down" color="#FF6F25" size={30} style={{ left: "47%", bottom: "1%" }} />
+          )}
+        </TouchableOpacity>
       </Animatable.View>
       {isModalVisible && (
         <Animatable.View
-        animation="zoomIn" // İstediğiniz animasyonu buraya ekleyin
-        duration={1000} // Animasyon süresini burada ayarlayın
-        style={styles.container}
-      >
-        
-        <View style={styles.modalContainer}>
+          animation="zoomIn"
+          duration={1000}
+          style={styles.container}
+        >
+          <View style={styles.modalContainer}>
             {showComments && (
-             <Animatable.View
-             animation="zoomInRight" // İstediğiniz animasyonu buraya ekleyin
-             duration={1500} // Animasyon süresini burada ayarlayın
-             style={styles.container}
-           >
-              <CommentList />
+              <Animatable.View
+                animation="zoomInRight"
+                duration={1500}
+                style={styles.container}
+              >
+                <CommentList item={items} />
               </Animatable.View>
-          )}
-          {!showComments && (
-            <>
-             <Animatable.View
-             animation="zoomInLeft" // İstediğiniz animasyonu buraya ekleyin
-             duration={1500} // Animasyon süresini burada ayarlayın
-             style={styles.container}
-           >
-              {items.type === "fitness_center" ? (
-                <Text style={styles.modalText}>Adres: {items.address} </Text>
-              ) : (
-                <Text style={styles.modalText}>Uzmanlık Alanı: {items.address} </Text>
-              )}
-              <Text style={styles.modalText}>Detayları: {items.description}</Text>
-              </Animatable.View>
-            </>
-          )}
-
-          <View style={{ flexDirection: "row", justifyContent: 'space-between' }}>
-            <TouchableOpacity onPress={handleCommentsPress} style={styles.commentButton}>
-            {showComments ? ( 
+            )}
+            {!showComments && (
               <>
-                <MaterialIcons name={"fitness-center"} color={"#0d0d0d"} size={20} style={{ position: "absolute", left: 10 }} />
-                <Text style={styles.commentText}>Salon Detayları</Text>
-                </>
-              ) : (
-              <>
-                <FontAwesome name={"comments"} color={"#0d0d0d"} size={20} style={{ position: "absolute", left: 20 }} />
-              <Text style={styles.commentText}>Yorumlar</Text>
+                <Animatable.View
+                  animation="zoomInLeft"
+                  duration={1500}
+                  style={styles.container}
+                >
+                  {items.type === "fitness_center" ? (
+                    <Text style={styles.modalText}>Adres: {items.address} </Text>
+                  ) : (
+                    <Text style={styles.modalText}>Uzmanlık Alanı: {items.address} </Text>
+                  )}
+                  <Text style={styles.modalText}>Detayları: {items.description}</Text>
+                </Animatable.View>
               </>
+            )}
 
+            <View style={{ flexDirection: "row", justifyContent: 'space-between', top: 5 }}>
+              <TouchableOpacity onPress={handleCommentsPress} style={styles.commentButton}>
+                {showComments ? (
+                  <>
+                    <MaterialIcons name={"fitness-center"} color={"#0d0d0d"} size={20} style={{ position: "absolute", left: 10 }} />
+                    <Text style={styles.commentText}>Salon Detayları</Text>
+                  </>
+                ) : (
+                  <>
+                    <FontAwesome name={"comments"} color={"#0d0d0d"} size={20} style={{ position: "absolute", left: 20 }} />
+                    <Text style={styles.commentText}>Yorumlar</Text>
+                  </>
+                )}
+              </TouchableOpacity>
+              {showComments ? (
+                <TouchableOpacity onPress={handleNewCommentPress} style={styles.commentButton}>
+                  <FontAwesome name={"comment"} color={"#0d0d0d"} size={20} style={{ position: "absolute", left: 20 }} />
+                  <Text style={styles.commentText}>Yorum Yap</Text>
+                </TouchableOpacity>
+              ) : (
+                <TouchableOpacity onPress={() => console.log("Yol Tarifi")} style={styles.commentButton}>
+                  <FontAwesome name={"location-arrow"} color={"#0d0d0d"} size={20} style={{ position: "absolute", left: 20 }} />
+                  <Text style={styles.commentText}>Yol Tarifi</Text>
+                </TouchableOpacity>
               )}
+            </View>
+            <TouchableOpacity onPress={closeModal} style={styles.closeButton}>
+              <FontAwesome name={"angle-double-up"} color={"#292929"} size={30} style={{ left: "1%", top: "1%" }} />
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={() => console.log("Yol Tarifi")} style={styles.commentButton}>
-              <FontAwesome name={"location-arrow"} color={"#0d0d0d"} size={20} style={{ position: "absolute", left: 20 }} />
-              <Text style={styles.commentText}>Yol Tarifi</Text>
-            </TouchableOpacity>
-          </View>
-          <TouchableOpacity onPress={closeModal} style={styles.closeButton}>
-            <FontAwesome name={"angle-double-up"} color={"#292929"} size={30} style={{ left: "1%", top: "1%" }} />
+           
+            <Modal visible={isCommentModalVisible} transparent animationType="fade">
+        <View style={styles.commentModalContainer}>
+          <Text style={styles.commentModalText}>Yorum Yap</Text>
+          <Text style={styles.commentNameText}> {userName} {lastName} </Text> 
+          <StarRating
+            disabled={false}
+            maxStars={5}
+            rating={rating}
+            selectedStar={(rating) => setRating(rating)}
+            fullStarColor="#FF6F25"
+            starSize={30}
+          />
+          <TextInput
+            style={styles.commentInput}
+            placeholder="Yorumunuzu buraya girin..."
+            value={newComment}
+            onChangeText={(text) => setNewComment(text)}
+            textAlignVertical="top"
+            multiline={true}
+            fontFamily="Arial"
+          />
+          <TouchableOpacity onPress={handleNewCommentSubmit} style={styles.submitButton}>
+            <Text style={styles.submitButtonText}>Gönder</Text>
           </TouchableOpacity>
-       
+          <TouchableOpacity onPress={() => setIsCommentModalVisible(false)} style={styles.closeCommentButton}>
+            <FontAwesome name={"close"} color={"#0D0D0D"} size={40} />
+          </TouchableOpacity>
         </View>
+      </Modal>
+
+          </View>
         </Animatable.View>
       )}
     </View>
@@ -154,6 +231,52 @@ const styles = StyleSheet.create({
     color: '#FFF',
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+
+  // Yorum yap popup stilleri
+  commentModalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  },
+  commentModalText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#FF6F25',
+    marginBottom: 15,
+  },
+  commentNameText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    marginBottom: 15,
+  },
+
+  commentInput: {
+    backgroundColor: '#FFFFFF',
+    width: '80%',
+    height: 100,
+    padding: 10,
+    borderRadius: 7,
+    marginBottom: 20,
+    marginTop:20,
+    textAlignVertical: 'top',
+  },
+  submitButton: {
+    backgroundColor: '#FF6F25',
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+    borderRadius: 7,
+  },
+  submitButtonText: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  },
+  closeCommentButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
   },
 });
 
