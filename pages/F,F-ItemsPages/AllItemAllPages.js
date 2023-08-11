76919b70c86607a64cc63896be5a36b601f19ecf
MustafaDemirhan <58307398+Mustafa6900@users.noreply.github.com>
2023-08-11 13:36:09 +0300

    font updated!

--- pages/F,F-ItemsPages/AllItemAllPages.js
+++ pages/F,F-ItemsPages/AllItemAllPages.js
@@ -187,7 +187,7 @@ const styles = StyleSheet.create({
   },
   noItem: {
     fontSize: 16,
-    fontWeight: '900',
+    fontWeight: '700',
     color: "#AAAAAA",
     marginLeft: "10%",
     marginRight: "10%",
