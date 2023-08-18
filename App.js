acbb809f63109bb22b0bca0f6d9eb009d82cfb37
MustafaDemirhan <58307398+Mustafa6900@users.noreply.github.com>
2023-08-18 20:36:45 +0300

    clean code

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
-import React,{useState,useEffect} from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { supabase } from './supabaseClient';
 import { AuthProvider } from './pages/Auth/AuthContext';
 import AuthStack from './pages/Auth/AuthStack';
 import { DataProvider } from './components/DataContext';
